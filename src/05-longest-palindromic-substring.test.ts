3f1f6ac47225708b1a6c5192e7ac37842897d105
Aleksandr Pozdnyakov <alexpozdnyakof@gmail.com>
2025-04-28 17:48:11 +0700

    fix(05): rewrite solution for additional test cases

--- src/05-longest-palindromic-substring.test.ts
+++ src/05-longest-palindromic-substring.test.ts
@@ -1,14 +1,28 @@
 import { test, expect } from 'vitest';
 import { longestPalindrome } from './05-longest-palindromic-substring';
 
-test('should return bab', () => {
+test('should return bab for babad', () => {
   expect(longestPalindrome('babad')).toBe('bab');
 });
 
-test('should return bb', () => {
+test('should return bb for cbbd', () => {
   expect(longestPalindrome('cbbd')).toBe('bb');
 });
 
-test('should return c', () => {
+test('should return c for c', () => {
   expect(longestPalindrome('c')).toBe('c');
+});
+
+test('should return a for ac', () => {
+  expect(longestPalindrome('ac')).toBe('a');
+});
+
+test('should return ccc for ccc', () => {
+  expect(longestPalindrome('ccc')).toBe('ccc');
+});
+
+test('should be the same for the long string from the same symbol repeated', () => {
+  const str =
+    'eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee';
+  expect(longestPalindrome(str)).toBe(str);
 });
--- src/05-longest-palindromic-substring.ts
+++ src/05-longest-palindromic-substring.ts
@@ -1,20 +1,18 @@
 export function longestPalindrome(s: string): string {
-  if (s.length < 2) return s;
   let result = '';
   let maxResult = '';
   let i = 0;
   let start = 0;
   while (i < s.length) {
-    result = result.concat(s[i]);
-    if (
-      result.length > 1 &&
-      result.at(0) === result.at(-1) &&
-      isPalindrome(result)
-    ) {
+    result += s[i];
+    if (result.length < maxResult.length) {
+      i++;
+      continue;
+    }
+    if (isPalindrome(result)) {
       maxResult = maxResult.length >= result.length ? maxResult : result;
-      result = '';
-      i = start++;
-    } else if (i === s.length - 1) {
+    }
+    if (i === s.length - 1) {
       result = '';
       i = start++;
     } else {
@@ -26,7 +24,7 @@ export function longestPalindrome(s: string): string {
 
 function isPalindrome(s: string): boolean {
   let i = (s.length / 2) << 0;
-  while (i !== 0) {
+  while (i >= 0) {
     if (s.at(i) !== s.at(-i - 1)) return false;
     i--;
   }
